8d11a54cab372a3fe903e9ae74430a87e5f77ca4
黄锋杰 <342586916@qq.com>
2025-12-24 16:31:57 +0800

    解决跨域问题配置类功能开发

---
+++ spzx-parent/spzx-manager/src/main/java/com/atguigu/spzx/manager/config/WebMvcConfiguration.java
@@ -0,0 +1,23 @@
+package com.atguigu.spzx.manager.config;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * 作者:hfj
+ * 功能:解决跨域问题配置类
+ * 日期: 2025/12/24 16:28
+ */
+@Component
+public class WebMvcConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")      // 添加路径规则
+                .allowCredentials(true)               // 是否允许在跨域的情况下传递Cookie
+                .allowedOriginPatterns("*")           // 允许请求来源的域规则
+                .allowedMethods("*")
+                .allowedHeaders("*") ;                // 允许所有的请求头
+    }
+}
