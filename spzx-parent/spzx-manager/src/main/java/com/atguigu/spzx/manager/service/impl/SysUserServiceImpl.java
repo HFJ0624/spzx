8417b97f52db4e6ce56286f19b3ae2a1d1f99b88
黄锋杰 <342586916@qq.com>
2025-12-24 17:12:08 +0800

    图片验证码校验的功能开发

--- spzx-parent/spzx-manager/src/main/java/com/atguigu/spzx/manager/service/impl/SysUserServiceImpl.java
+++ spzx-parent/spzx-manager/src/main/java/com/atguigu/spzx/manager/service/impl/SysUserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.atguigu.spzx.manager.service.impl;
 
+import cn.hutool.core.util.StrUtil;
 import com.alibaba.fastjson.JSON;
 import com.atguigu.spzx.common.exception.GuiguException;
 import com.atguigu.spzx.manager.mapper.SysUserMapper;
@@ -33,6 +34,21 @@ public class SysUserServiceImpl implements SysUserService {
     //用户登录
     @Override
     public LoginVo login(LoginDto loginDto) {
+
+        //1.获取输入的验证码和存储在redis的key名称
+        String captcha = loginDto.getCaptcha();// 用户输入的验证码
+        String key = loginDto.getCodeKey();// redis中验证码的数据key
+
+        //2.从Redis中获取验证码
+        String redisCode = redisTemplate.opsForValue().get("user:validate" + key);
+
+        //3.比较输入的验证码和redis里面的验证码是否一致
+        if (StrUtil.isEmpty(redisCode) || !StrUtil.equalsIgnoreCase(redisCode,captcha)){
+            throw new GuiguException(ResultCodeEnum.VALIDATECODE_ERROR);
+        }
+
+        //4.如果一致，删除redis里面的验证码
+        redisTemplate.delete("user:validate" + key);
 
         //1.获取提交的用户名，loginDto获取
         String userName = loginDto.getUserName();
