06f29500a17e54df7fe9a13cd032d30e8077265b
黄锋杰 <342586916@qq.com>
2025-12-25 08:32:50 +0800

    角色列表功能的开发

---
+++ spzx-parent/spzx-manager/src/main/java/com/atguigu/spzx/manager/controller/SysRoleController.java
@@ -0,0 +1,32 @@
+package com.atguigu.spzx.manager.controller;
+
+import com.atguigu.spzx.manager.service.SysRoleService;
+import com.atguigu.spzx.model.dto.system.SysRoleDto;
+import com.atguigu.spzx.model.entity.system.SysRole;
+import com.atguigu.spzx.model.vo.common.Result;
+import com.atguigu.spzx.model.vo.common.ResultCodeEnum;
+import com.github.pagehelper.PageInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * 作者:hfj
+ * 功能:角色功能实现
+ * 日期: 2025/12/24 21:13
+ */
+@RestController
+@RequestMapping(value = "/admin/system/sysRole")
+public class SysRoleController {
+
+    @Autowired
+    private SysRoleService sysRoleService;
+
+    //角色列表的查询方法
+    @PostMapping("/findByPage/{current}/{limit}") //current:当前页 limit:每页显示的数量 sysRoleDto:条件角色名称对象
+    public Result findByPage(@PathVariable("current") Integer current, @PathVariable("limit") Integer limit, @RequestBody SysRoleDto sysRoleDto){
+
+        //pageHelper插件实现分页
+        PageInfo<SysRole> pageInfo = sysRoleService.findByPage(sysRoleDto,current,limit);
+        return Result.build(pageInfo, ResultCodeEnum.SUCCESS);
+    }
+}
---
+++ spzx-parent/spzx-manager/src/main/java/com/atguigu/spzx/manager/service/impl/SysRoleServiceImpl.java
@@ -0,0 +1,39 @@
+package com.atguigu.spzx.manager.service.impl;
+
+import com.atguigu.spzx.manager.mapper.SysRoleMapper;
+import com.atguigu.spzx.manager.service.SysRoleService;
+import com.atguigu.spzx.model.dto.system.SysRoleDto;
+import com.atguigu.spzx.model.entity.system.SysRole;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * 作者:hfj
+ * 功能:
+ * 日期: 2025/12/24 21:13
+ */
+@Service
+public class SysRoleServiceImpl implements SysRoleService {
+
+    @Autowired
+    private SysRoleMapper sysRoleMapper;
+
+    //角色列表的查询方法
+    @Override
+    public PageInfo<SysRole> findByPage(SysRoleDto sysRoleDto, Integer current, Integer limit) {
+
+        //设置分页参数
+        PageHelper.startPage(current,limit);
+
+        //根据条件查询所有数据
+        List<SysRole> list = sysRoleMapper.findByPage(sysRoleDto);
+
+        //封装pageInfo对象
+        PageInfo<SysRole> pageInfo = new PageInfo<>(list);
+        return pageInfo;
+    }
+}
