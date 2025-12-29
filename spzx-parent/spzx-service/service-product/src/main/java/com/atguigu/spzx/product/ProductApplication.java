c8cddcbc5b294c9aa1c0352dc55fa1d569dc2303
黄锋杰 <342586916@qq.com>
2025-12-29 17:06:02 +0800

    部署前端工程和搭建后端工程

---
+++ spzx-parent/spzx-service/service-product/src/main/java/com/atguigu/spzx/product/ProductApplication.java
@@ -0,0 +1,17 @@
+package com.atguigu.spzx.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 作者:hfj
+ * 功能:商品启动类
+ * 日期: 2025/12/29 17:04
+ */
+@SpringBootApplication
+public class ProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductApplication.class, args);
+    }
+}
